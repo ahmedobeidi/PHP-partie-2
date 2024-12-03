05cf8558a1054828fd7d0587099e3e1dc1c6c2ef
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-03 15:23:03 +0100

    DONE: Exercise 3

--- alog.php
+++ alog.php
@@ -2,7 +2,7 @@
 
 /* Exercice 1 
 $age = 18;
-if ($age <= 18) echo "Vous êtes majeur";
+if ($age >= 18) echo "Vous êtes majeur";
 else echo "Vous êtes mineur";
 */
 
@@ -10,4 +10,22 @@ else echo "Vous êtes mineur";
 $isEasy = true;
 if ($isEasy) echo "C'est facile!!";
 else echo " C'est difficile !!!";
+*/
+
+/* Exercice 3 
+$age = 29;
+$gender = "Homme";
+
+if ($age >= 18 && $gender === "Homme") {
+    echo "Vous êtes un homme et vous êtes majeur";
+}
+else if ($age < 18 && $gender === "Homme") {
+    echo "Vous êtes un homme et vous êtes mineur";
+}
+else if ($age >= 18 && $gender === "Femme") {
+    echo "Vous êtes une femme et vous êtes majeur";
+}
+else {
+    echo "Vous êtes une femme et vous êtes mineur";
+}
 */
