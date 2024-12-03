3b2dfd20b5dad9ccb31ca17fdedeed13ec72bbe7
ahmedobeidi <alobeidi.dev@gmail.com>
2024-12-03 15:45:47 +0100

    DONE: Exercise 6

--- alog.php
+++ alog.php
@@ -29,3 +29,63 @@ else {
     echo "Vous êtes une femme et vous êtes mineur";
 }
 */
+
+/* Exercice 4 
+$magnitude = 3;
+$result;
+
+switch ($magnitude) {
+    case 1:
+       $result = "Micro-séisme impossible à ressentir.";
+       break; 
+    case 2:
+        $result = "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres."; 
+        break; 
+    case 3:
+        $result = "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.";
+        break; 
+    case 4:
+        $result = "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.";
+        break; 
+    case 5:
+        $result = "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.";
+        break; 
+    case 6:
+        $result = "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre."; 
+        break;
+    case 7:
+        $result = "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.";
+        break; 
+    case 8:
+        $result = "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres";
+        break;
+    case 9:
+        $result = "Séisme capable de tout détruire sur une très vaste zone.";
+        break; 
+}
+echo $result;
+*/
+
+/* Exercice 5 
+$gender = "Homme";
+
+if ($gender != "Homme") {
+    echo 'C\'est une développeuse !!!';
+}
+else {
+    echo 'C\'est un développeur !!!';
+}
+*/
+
+/* Exercice 6 
+$age = 17;
+
+if ($age >= 18) {
+    echo 'Tu es majeur';
+}
+else {
+    echo 'Tu n\'es pas majeur';
+}
+*/
+
+/* Exercice 7 */
